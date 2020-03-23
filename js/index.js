a65f9d3dc8c6d0f2b32a07c88e10725c061d2001
Suyeon Kang <tndusrkd92@gmail.com>
2020-03-23 21:37:53 +0900

    Add style to navigation and header, and create a section- activityn
    Create a round hamburger menu and change some style of navigation

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/*
+document.querySelector('.navbar__menu-container').addEventListener('click', function() {
+    document.querySelector('.navigation').style.display = 'block';
+});
+*/
+
+/*
+$('.navbar__menu-container').click(function() {
+    $('.navigation').slideToggle();
+});
+*/
+
+
+function show() {
+    document.querySelector('.navigation').classList.toggle('active');
+};
