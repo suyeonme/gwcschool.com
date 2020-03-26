03f86346e1f64798f887936ecb051fd85e2ac000
Suyeon Kang <tndusrkd92@gmail.com>
2020-03-26 21:28:34 +0900

    Create school-about section, gallery section and footer and link about.html.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,4 @@
-/*
-document.querySelector('.navbar__menu-container').addEventListener('click', function() {
-    document.querySelector('.navigation').style.display = 'block';
-});
-*/
-
-/*
-$('.navbar__menu-container').click(function() {
-    $('.navigation').slideToggle();
-});
-*/
-
-
+// Navigation toggle
 function show() {
     document.querySelector('.navigation').classList.toggle('active');
 };
