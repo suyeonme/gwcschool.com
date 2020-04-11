2c6500a66691a028b61bdd118c8daae49d7b1eda
Suyeon Kang <tndusrkd92@gmail.com>
2020-04-11 17:00:37 +0900

    Create facility.html and complete location.html

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 // Navigation toggle
-
 function show() {
     document.querySelector('.navigation').classList.toggle('active');
 };
+
+// KAKAO MAP
+new daum.roughmap.Lander({
+    "timestamp" : "1586588048930",
+    "key" : "xvj6",
+    "mapWidth" : "700",
+    "mapHeight" : "360"
+}).render();
