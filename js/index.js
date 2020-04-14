6af243b34e55809cf7eea176ab83099021713df2
Suyeon Kang <tndusrkd92@gmail.com>
2020-04-14 16:33:37 +0900

    Implement animation.css, waypoints.css and scrollie. And add a functionality in facilities.html
    Add a functionality which change background-color while scrolling using scrolli

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
-// Navigation toggle
+
+// NAVIGATION TOGGLE
 function show() {
     document.querySelector('.navigation').classList.toggle('active');
 };
 
+// BACKGROUND CHANGE WHILE SCROLLING
+$( window ).ready(function() {
+    var wHeight = $(window).height();
+    
+    $('.slide')
+    .height(wHeight)
+    .scrollie({
+        scrollOffset : -50,
+        scrollingInView : function(elem) {
+        var bgColor = elem.data('background');
+        
+        $('body').css('background-color', bgColor);
+        }
+    });
+});
+
+// WAYPOINTS
+$('#wp-1').waypoint(function (direction) {
+    $('#wp-1').addClass('animated fadeIn');
+}, {
+    offset: '60%'
+});
+
+
 // KAKAO MAP
+/*
 new daum.roughmap.Lander({
     "timestamp" : "1586588048930",
     "key" : "xvj6",
     "mapWidth" : "700",
     "mapHeight" : "360"
 }).render();
+*/
