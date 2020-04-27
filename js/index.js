378a7d059f133bbd580f0dacddbcc20a629712cc
Suyeon Kang <tndusrkd92@gmail.com>
2020-04-27 21:02:23 +0900

    Apply media query to contactUs.html

--- js/index.js
+++ js/index.js
@@ -23,12 +23,12 @@ $(window).ready(function () {
 
 /////////////////////////////////////
 // KAKAO MAP
+
 new daum.roughmap.Lander({
-    "timestamp": "1586588048930",
-    "key": "xvj6",
-    "mapWidth": "700",
-    "mapHeight": "360"
+    "timestamp": "1587986189284",
+    "key": "y5rn"
 }).render();
+
 
 /////////////////////////////////////
 // WAYPOINTS
